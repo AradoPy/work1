a918474f862229343e8c7e8521aaba94d94764ef
Ilya <ilya.kuznetsov.ilya@gmail.com>
2021-03-30 16:22:41 +0300

    Make list2geojson function

--- Excode.py
+++ Excode.py
@@ -17,9 +17,27 @@ def reader():
     for i in range(1, 129):
             a = sheet.cell(row=i+1, column=3).value
             Y.append(float(a))
-    return x, y
+    coor = zip(Y, X)
+    list_coor = list(coor)
+    return list_coor
 
+def list2geojson(list):
+    features = []
+    for i in list:
+        tupe_road = ["urb", "rur", "hway"]
+        width_road = [5, 7, 10, 12, 18]
+        long_road = rd.randint(100, 150)
+        surface = ["asp", "crt", "gra", "grnd"]
+        features.append(jsn.Feature(geometry=jsn.Point(i), properties={"type": rd.choice(tupe_road),
+                                                                       "width": rd.choice(width_road),
+                                                                       "long": long_road,
+                                                                       "face": rd.choice(surface),
+                                                                       }))
+    feature_collection = jsn.FeatureCollection(features=features)
+    with open('All_points.geojson', 'w') as f:
+        jsn.dump(feature_collection, f)
+    with open('JSON_for_DB.json', 'w') as f:
+        jsn.dump(features, f)
 
-x, y = reader()
-coor = zip(Y, X)
-list_coor = list(coor)
+list = reader()
+list2geojson(list)
